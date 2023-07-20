4824e2c7fdd78c1db5c1dd11c671c12fe5863aac
Demetrio Carrara <demetrio.carrara@tx.group>
2023-07-20 16:57:32 +0200

    fix curly brackets again

--- templates/user_data-ubuntu.sh
+++ templates/user_data-ubuntu.sh
@@ -31,8 +31,9 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y \
 YQ_VERSION=v4.34.1
 yq_architecture=""
 case $(uname -m) in
-    x86_64) yq_architecture="amd64" ;;
-    arm)    yq_architecture="arm64" ;;
+    x86_64)  yq_architecture="amd64" ;;
+    arm)     yq_architecture="arm64" ;;
+    aarch64) yq_architecture="arm64" ;;
 esac
 YQ_BINARY="yq_linux_${yq_architecture}"
 wget https://github.com/mikefarah/yq/releases/download/$YQ_VERSION/$YQ_BINARY.tar.gz -O - |\
