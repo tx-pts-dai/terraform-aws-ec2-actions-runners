cdbbb541ccecfb3a635b4133d5649027a4a58029
Demetrio Carrara <demetrio.carrara@tx.group>
2023-09-07 13:04:45 +0200

    deps: update to v3.6.1

--- lambdas/runners_lambdas.tf
+++ lambdas/runners_lambdas.tf
@@ -1,18 +1,18 @@
 module "lambdas" {
-  version = "v3.2.0"
+  version = "v3.6.1"
   source  = "philips-labs/github-runner/aws//modules/download-lambda"
   lambdas = [
     {
       name = "webhook"
-      tag  = "v3.2.0"
+      tag  = "v3.6.1"
     },
     {
       name = "runners"
-      tag  = "v3.2.0"
+      tag  = "v3.6.1"
     },
     {
       name = "runner-binaries-syncer"
-      tag  = "v3.2.0"
+      tag  = "v3.6.1"
     }
   ]
 }
