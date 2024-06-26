f9845a3e15a75f14b75529a8923fde76fec587fc
Demetrio Carrara <demetrio.carrara@tx.group>
2024-06-26 15:52:09 +0200

    align packages installation

--- templates/user_data-al2023.sh
+++ templates/user_data-al2023.sh
@@ -31,20 +31,20 @@ dnf install -y --allowerasing curl
   
 
 # Install 'yq'
-# YQ_VERSION=v4.34.1
-# yq_architecture=""
-# case $(uname -m) in
-#     x86_64)  yq_architecture="amd64" ;;
-#     arm)     yq_architecture="arm64" ;;
-#     aarch64) yq_architecture="arm64" ;;
-# esac
-# YQ_BINARY="yq_linux_$yq_architecture"
-# wget https://github.com/mikefarah/yq/releases/download/$YQ_VERSION/$YQ_BINARY.tar.gz -O - |\
-#   tar xz && mv $YQ_BINARY /usr/bin/yq
+YQ_VERSION=v4.34.1
+yq_architecture=""
+case $(uname -m) in
+    x86_64)  yq_architecture="amd64" ;;
+    arm)     yq_architecture="arm64" ;;
+    aarch64) yq_architecture="arm64" ;;
+esac
+YQ_BINARY="yq_linux_$yq_architecture"
+wget https://github.com/mikefarah/yq/releases/download/$YQ_VERSION/$YQ_BINARY.tar.gz -O - |\
+  tar xz && mv $YQ_BINARY /usr/bin/yq
 
-# echo  'LANG="en_US.UTF-8"' >>  /etc/environment
-# echo  'LC_ALL="en_US.UTF-8"' >>  /etc/environment
-# echo  'LC_CTYPE="en_US.UTF-8"' >>  /etc/environment
+echo  'LANG="en_US.UTF-8"' >>  /etc/environment
+echo  'LC_ALL="en_US.UTF-8"' >>  /etc/environment
+echo  'LC_CTYPE="en_US.UTF-8"' >>  /etc/environment
 
 USER_NAME=runners
 useradd -m -s /bin/bash $USER_NAME
@@ -56,10 +56,10 @@ service docker start
 usermod -aG docker $USER_NAME
 
 # Auto-login to DockerHub if credentials are passed in the pre_install step
-# if [ "$DOCKERHUB_USERNAME" != "" ] && [ "$DOCKERHUB_PASSWORD" != "" ]; then
-#     echo "DockerHub username and password detected. Logging in automatically..."
-#     su $USER_NAME -c "docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD"
-# fi
+if [ "$DOCKERHUB_USERNAME" != "" ] && [ "$DOCKERHUB_PASSWORD" != "" ]; then
+    echo "DockerHub username and password detected. Logging in automatically..."
+    su $USER_NAME -c "docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD"
+fi
 
 echo "INFO: installing github runner at $(date -u +%H:%M:%S)"
 
