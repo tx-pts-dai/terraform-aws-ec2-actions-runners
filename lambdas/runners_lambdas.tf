79f79c0251918a5a5ac52d1e6077fcccd0557bd9
Demetrio Carrara <demetrio.carrara@tx.group>
2024-06-26 08:43:51 +0200

    downgrade to 5.10.4

--- lambdas/runners_lambdas.tf
+++ lambdas/runners_lambdas.tf
@@ -1,18 +1,18 @@
 module "lambdas" {
-  version = "v5.11.0"
+  version = "5.10.4"
   source  = "philips-labs/github-runner/aws//modules/download-lambda"
   lambdas = [
     {
       name = "webhook"
-      tag  = "v5.11.0"
+      tag  = "v5.10.4"
     },
     {
       name = "runners"
-      tag  = "v5.11.0"
+      tag  = "v5.10.4"
     },
     {
       name = "runner-binaries-syncer"
-      tag  = "v5.11.0"
+      tag  = "v5.10.4"
     }
   ]
 }
