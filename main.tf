ece3831f4697391d06862de754bd651c0d7b35a3
Eric Funk <93534965+ericfunktxgroup@users.noreply.github.com>
2024-10-30 09:24:55 +0100

    improve description

--- main.tf
+++ main.tf
@@ -95,8 +95,8 @@ locals {
       # if runners are ephemeral, scale-down function it's not needed because runners should get destroyed by itself after the run.
       # therefore we set it to once every 30 min
       scale_down_schedule_expression = runner.ephemeral ? "cron(*/30 * * * ? *)" : null
-      # many worksflows are split accross multiple jobs spaning a new runner immitatly after the finish of the current one
-      # setting a minimum of 15 minutes to avoid restarting runners too often
+      # Many workflows are divided across multiple jobs, each triggering a new runner immediately after the previous one finishes.
+      # Set a minimum delay of 15 minutes to prevent runners from provisioning too frequently.
       minimum_running_time_in_minutes = 15
     })
 
