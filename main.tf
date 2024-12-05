318b18ea504d0ed58b2cf603bb8be92208af0080
Eric Funk <eric.funk@tamedia.ch>
2024-12-05 17:32:42 +0100

    use noble (latest ubuntu LTS version)

--- main.tf
+++ main.tf
@@ -18,8 +18,8 @@ locals {
     ubuntu = {
       owners = ["099720109477"] # canonical
       filters = {
-        "arm64" = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-arm64-server-*"]
-        "x64"   = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+        "arm64" = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-arm64-server-*"]
+        "x64"   = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
       }
     }
   }
